954f8f64de376f14e8cb3b0b8eb7528603c185fb
Jo Tshiba <inktamba@gmail.com>
2026-08-14 19:32:47 +0200

    oooo

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Nette\Schema\ValidationException;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
