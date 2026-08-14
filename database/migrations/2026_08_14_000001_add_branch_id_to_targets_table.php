29a8f8c589698984b518abc23a62da1e5d3a5e22
Jo Tshiba <inktamba@gmail.com>
2026-08-14 19:24:56 +0200

    ok

--- database/migrations/2026_08_14_000001_add_branch_id_to_targets_table.php
+++ database/migrations/2026_08_14_000001_add_branch_id_to_targets_table.php
@@ -11,12 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('targets', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-            $table->string('name');
-            $table->integer('skill');
-            $table->text('bio');
+        Schema::table('targets', function (Blueprint $table) {
+            $table->foreignId('branch_id')->nullable()->constrained()->onDelete('cascade');
         });
     }
 
@@ -25,6 +21,9 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('targets');
+        Schema::table('targets', function (Blueprint $table) {
+            $table->dropForeign(['branch_id']);
+            $table->dropColumn('branch_id');
+        });
     }
 };
