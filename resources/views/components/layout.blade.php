e3c117390412318b496665d9150b06a93280f55b
Jo Tshiba <inktamba@gmail.com>
2026-08-18 10:23:16 +0200

    img

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,6 +3,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home | Agents</title>
+    <link rel="icon" type="image/jpeg" href="resources/img/agentsweb.jpg">
 
     @vite('resources/css/app.css')
 </head>
